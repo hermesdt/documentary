64629d17723596ae0cff3078fcf78302fb0c6576
Álvaro Durán Tovar <hermesdt@gmail.com>
2014-01-05 21:31:23 +0100

    started with the pagination

--- teledocumentales.js
+++ teledocumentales.js
@@ -5,23 +5,58 @@ var Encoder = require('node-html-encoder').Encoder;
 
 var encoder = new Encoder('entity');
 
+exports.fetch_all = function(){
+  
+}
+
+function fetch_page(page_url){
+  html.download(page_url, function(txt){
+    var lines = txt.split("\n");
+    var videos_section_started = false;
+    var h3_started = false;
+    var urls = [];
+
+    for(i = 0;i<lines.length;i++){
+      var line = lines[i];
+      if(line.match(/<div id="izquierda">/)){
+        videos_section_started = true;
+        continue;
+      }
+
+      if(line.match(/<h3>/){
+        h3_started = true;
+        continue;
+      }
+
+      if(videos_section_started && h3_started && line.match(/<a href=/)){
+        var href = line.match(//);
+      }
+    }
+  });
+}
+
 exports.get_iframe_url = function(url, callback){
   get_embed_iframe_url(url, function(video, embed_url){
-    get_iframe_by_provider(embed_url, function(final_url){
+    video.setUrl(embed_url);
+    callback(video);
+
+    /*get_iframe_by_provider(embed_url, function(final_url){
       video.setUrl(final_url);
       callback(video);
-    });
+    });*/
   });
 };
 
 function get_embed_iframe_url(url, callback){
   html.download(url, function(txt){
-    var regexp = /end navigation -->\s+<iframe.*src=.(.*). frameborder/;
-    var match = txt.match(regexp);
-    if(match != null){
-      var attributes = get_attributes_from_text(txt);
-      var video = new Video(attributes);
-
+    var attributes = get_attributes_from_text(txt);
+    var video = new Video(attributes);
+    video.attributes.title = txt.match(/<h1><span class="flecha">&raquo;<\/span> (.*)<\/h1>/)[1];
+    if(txt.match(/end navigation -->\s+<iframe.*src=.(.*). frameborder/)){
+      var match = txt.match(/end navigation -->\s+<iframe.*src=.(.*). frameborder/);
+      callback(video, match[1]);
+    }else if(txt.match(/end navigation -->\s+<div id="mediaspace">Reproductor Flash<\/div>/)){
+      var match = txt.match(/so.addVariable\('playlistfile','(.*)'\);/);
       callback(video, match[1]);
     }else{
       var tag = txt.match(/end navigation -->\s+<(.*)>(.*)<\/(.*)/);
@@ -33,6 +68,8 @@ function get_embed_iframe_url(url, callback){
 function get_attributes_from_text(txt){
   var attributes = {};
   var txt = encoder.htmlDecode(txt);
+  txt = txt.replace(/<br\s*\/>/, "");
+  txt = txt.replace(/<p>/g, "").replace(/<\/p>/g, "");
 
   var lines = txt.match(/id="informax">([\s\S]*)<\/div>\s+<\/div>\s+<\/div>\s+<div style="clear:both">/)[1].split("\n");
   var readingSinopsis = false;
@@ -40,13 +77,15 @@ function get_attributes_from_text(txt){
   lines.forEach(function(line){
     if(readingSinopsis && line.match("<strong>")){
       readingSinopsis = false;
+      sinopsis = sinopsis.replace("<div>", "");
       attributes["Sinopsis"] = sinopsis;
     }
 
-    if(readingSinopsis && line.match("<p>")){
-      sinopsis += line.match(/<p>(.*)<\/p>/)[1] + "\n";
+    if(readingSinopsis){
+      // sinopsis += line.match(/<p>(.*)(<\/p>)?/)[1] + "\n";
+      sinopsis += line + "\n";
     }else if(line.match("<strong>")){
-      var match = line.match(/<strong>(.*): <\/strong>(.*)<\/p>/);
+      var match = line.match(/<strong>(.*): <\/strong>(.*)/);
       if(match[1] != "Visitas" && match[1] != "Fuente")
         if(match[1] == "Sinopsis")
           readingSinopsis = true;
